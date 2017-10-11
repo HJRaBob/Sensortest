d41f38da727b4371a6c57e7d393042ff388e6bf9
HJRabob <kidma222@naver.com>
2017-10-11 14:34:41 +0900

    data base form

--- data_base.py
+++ data_base.py
@@ -4,15 +4,26 @@ class Group_two_DB:
     def __init__(self):
         self.conn = sqlite3.connect('sensor.db')
         self.curs = self.conn.cursor()
+        self.make_table('Jog')
+        self.make_table('LED')
+        self.make_table('CLCD')
+        self.make_table('FND')
+        self.make_table('Motor')
+
+        self.make_table('PIR')
+        self.make_table('Light')
+        self.make_table('Temp')
+        self.make_table('Piezo')
+        self.make_table('Sonic')
         return
 
-    def make_table(self,name,field):
+    def make_table(self,name):
         self.curs.execute('drop table if exists {}'.format(name))
-        self.curs.execute('create table {}({})'.format(name,field))
+        self.curs.execute('create table {}(time integer primary key,state varchar(10),commend varchar(10))'.format(name))
         return
 
-    def add_values(self,table_name,form,values):
-        self.curs.executemany('insert into {} values {}'.format(table_name,form),values)
+    def add_values(self,table_name,values):
+        self.curs.execute('insert into {} values {}'.format(table_name,values))
         return
 
     def save(self):
@@ -23,10 +34,8 @@ class Group_two_DB:
         self.conn.close()
         return
 
-"""test code"""
+
 two_db = Group_two_DB()
-two_db.make_table('store','try,number')
-test = [(1,100),(2,200),(3,300),(4,400),(5,500)]
-two_db.add_values('store','(?,?)',test)
+
 two_db.save()
 two_db.finish()
