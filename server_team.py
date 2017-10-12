68a7205505f1dbe7ba52f956d85149638b834f3d
puppenhaus <32695035+puppenhaus@users.noreply.github.com>
2017-10-12 16:23:14 +0900

    Update server_team.py

--- server_team.py
+++ server_team.py
@@ -1,6 +1,6 @@
 import socket
 #import sys
-import RPi.GPIO as GPIO 
+#import RPi.GPIO as GPIO 
 from time import sleep
 #import GUIserver as mylib
 #import pymysql
@@ -8,20 +8,24 @@ from time import sleep
 class Socket:
     def __init__(self, data):
 
+        HOST='localhost'
+        PORT=8001
         self.svrsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.svrsock.bind(('localhost', 8001))
-        self.svrsock.listen(0)
+        self.svrsock.bind(('HOST', PORT))
+        self.svrsock.listen(5)
+        self.svrsock.connect('HOST', PORT)
 
 
-        self.clientsock, addr = svrsock.accept()
-        self.data = clientsock.recv(65535)
-        self.bdsock.send(data)
+        conn, addr = svrsock.accept()
+        data = clientsock.recv(65535)
+        clientsock.send(data)
 
         print(data.decode())
         print(addr[0], addr[1])
 
         while True:
             data = conn.recv(1024)
+            print(data)
             if not data: break
             conn.send(data)
         conn.close()
@@ -30,42 +34,3 @@ class Socket:
     def split_word(data):
         words = data.split()
         print(words)
-
-
-
-
-
-
-
-
-
-"""            
-#connect = pymysql.connect(db='data_base')
-#curs = connect.cursor()
-#data = (('JOG_ON', 1, 'JOG'), ('JOG_OFF', 0, 'JOG'),
-#        ('LED_ON', 1, 'LED'), ('LED_OFF', 0, 'LED')
-#        ('MOTOR_ON', 1, 'MOTOR'), ('MOTOR_OFF', 0, 'MOTOR')
-#        ('PIR_ON', 1, 'PIR'), ('PIR_OFF', 0, 'PIR')
-#        ('ILLU_ON', 1, 'ILLU'), ('ILLU_OFF', 0, 'ILLU')
-#        ('TEMP_ON', 1, 'TEMP'), ('TEMP_OFF', 0, 'TEMP')
-#        ('PIEZO_ON', 1, 'PIEZO'), ('PIEZO_OFF', 0, 'PIEZO')
-#        ('SONIC_ON', 1, 'SONIC'), ('SONIC_OFF', 0, 'SONIC'))
-#sql = ""insert into data() values (%s, %s)""
-#curs.execute(sql, data)
-#Socket(data)
-#curs.execute("insert into values ('table_name, :state);" (table_name, state))
-#state = [('JOG', 54), ('LED', 34), ('MOTOR', 44), ('PIR', ), ('ILLU', ),
-#          ('TEMP', ), ('PIEZO', ), ('SONIC', )]
-#curs.executemany('insert into employee values(?,?)', values)
-
-#connect.commit()
-#connect.close()
-
-#    def send(self):
-
-#    def data(self):
-#        LED
-#        LCD
-#        PIEZO
-#        MOTOR
-"""
